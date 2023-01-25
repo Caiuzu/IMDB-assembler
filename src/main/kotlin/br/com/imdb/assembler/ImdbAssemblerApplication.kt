c64151b78c256234f25cb893ccd27b34879a2886
Caio Souza <caio.7s@outlook.com>
2023-01-25 17:26:59 -0300

    feat: Inicializando projeto

---
+++ src/main/kotlin/br/com/imdb/assembler/ImdbAssemblerApplication.kt
@@ -0,0 +1,6 @@
+package br.com.imdb.assembler
+
+fun main() {
+    val movieApplication = MovieApplication()
+    movieApplication.run()
+}
---
+++ src/test/kotlin/br/com/imdb/assembler/ImdbAssemblerApplicationTests.kt
@@ -0,0 +1,9 @@
+package br.com.imdb.assembler
+
+class ImdbAssemblerApplicationTests {
+
+//    @Test
+//    fun contextLoads() {
+//    }
+
+}
